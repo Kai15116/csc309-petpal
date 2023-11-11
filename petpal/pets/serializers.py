8344ce4f3f54137c1d5bec4a7341cdb0eb9af7b6
kai <kai.dicarlantonio@mail.utoronto.ca>
2023-11-11 00:59:27 -0500

    Implemented initial implementation for pets features in the handout

---
+++ petpal/pets/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework.serializers import ModelSerializer, FloatField, DecimalField, DateTimeField
+
+from .models import Pet
+import copy
+
+
+class PetSerializer(ModelSerializer):
+
+    class Meta:
+        model = Pet
+        exclude = ['id', 'last_modified', 'owner']
+        read_only_fields = ['id', 'last_modified', 'owner']
+
+
+class PetSearchSerializer(ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for key in self.get_fields():
+            field = self.fields[key]
+            field.required = False
+
+            if isinstance(field, (FloatField, DecimalField, DateTimeField)):
+                self.fields[f'{key}__gt'] = copy.deepcopy(field)
+                self.fields[f'{key}__gte'] = copy.deepcopy(field)
+                self.fields[f'{key}__lt'] = copy.deepcopy(field)
+                self.fields[f'{key}__lte'] = copy.deepcopy(field)
+
+                self.fields[f'{key}__gt'].required = False
+                self.fields[f'{key}__gte'].required = False
+                self.fields[f'{key}__lt'].required = False
+                self.fields[f'{key}__lte'].required = False
+
+    class Meta:
+        model = Pet
+        exclude = ['id', 'name', 'adoption_location', 'medical_history', 'notes', 'picture_1', 'picture_2', 'picture_3']
--- petpal/pets/views.py
+++ petpal/pets/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, get_object_or_404, ListAPIView
+from rest_framework.permissions import SAFE_METHODS
+from .models import Pet
+from .serializers import PetSerializer, PetSearchSerializer
+
+from accounts.permissions import IsPetShelterOrReadOnly
 
 # Create your views here.
+
+
+class ListCreatePetView(ListCreateAPIView):
+    serializer_class = PetSerializer
+    permission_classes = [IsPetShelterOrReadOnly]
+
+    def perform_create(self, serializer):
+        Pet.objects.create(**serializer.validated_data, owner=self.request.user.petshelter)
+
+    def get_queryset(self):
+        serializer = PetSearchSerializer(data=self.request.query_params)
+        serializer.is_valid(raise_exception=True)
+        return Pet.objects.all().filter(**serializer.validated_data)
+
+
+class RetrieveUpdateDestroyPetView(RetrieveUpdateDestroyAPIView):
+    serializer_class = PetSerializer
+
+    def get_queryset(self):
+        if self.request.method in SAFE_METHODS:
+            return Pet.objects.all()
+        return Pet.objects.filter(owner=self.request.user)
