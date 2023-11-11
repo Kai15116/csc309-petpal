74e6e54fa98816a82dec315bc08ea3007382736a
kai <kai.dicarlantonio@mail.utoronto.ca>
2023-11-11 00:59:27 -0500

    Implemented initial implementation for pets features in the handout

--- petpal/petpal/urls.py
+++ petpal/petpal/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('pets/', include('pets.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
