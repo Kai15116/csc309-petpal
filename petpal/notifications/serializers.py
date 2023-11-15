e8c6fc00fb99eb9a7271f27ada0d79857ff63503
kai <kai.dicarlantonio@mail.utoronto.ca>
2023-11-15 03:36:32 -0500

    Added fix to notification create

--- petpal/notifications/serializers.py
+++ petpal/notifications/serializers.py
@@ -3,39 +3,43 @@ from accounts.models import PetSeeker, PetShelter
 from applications.models import Application
 from pets.models import Pet
 from .models import Notification
-from tokenize import Comment
+from comments.models import Comment
+
+
+# from tokenize import Comment
+
 
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ['id', 'user', 'created_at', 'read', 'content_type', 'object_id', 'content_object', 'notification_type']
+        fields = ['id', 'user', 'created_at', 'read', 'content_type', 'object_id', 'notification_type']
         read_only_fields = ['id', 'user', 'created_at', 'read']
 
-    def create(self, validated_data):
-        content_object = validated_data['content_object']
-        user = self.context['request'].user  
-
-        if isinstance(user, PetSeeker) and isinstance(content_object, Application):
-            notification_type = 'status_update'
-        
-        # if user is PetShelter AND content_object refers to Application => application creation notification
-        elif isinstance(user, PetShelter) and isinstance(content_object, Application):
-            notification_type = 'application_creation'
-
-        # if user is PetShelter AND content_object refers to Comment AND comment.content_object refers to Shelter
-        elif isinstance(user, PetShelter) and isinstance(content_object, Comment) and isinstance(content_object.content_object, PetShelter):
-            notification_type = 'new_review'
-        
-        # if content_object refers to Comment AND comment.content_object refers to Application => new message notification
-        elif isinstance(content_object, Comment) and isinstance(content_object.content_object, Application):
-            notification_type = 'new_message'
-            
-        # if content_object refers to Pet => new pet listing notification
-        elif isinstance(content_object, Pet):
-            notification_type = 'new_pet_listing'
-        
-        # if no conditions apply set an empty value
-        else:
-            notification_type = ''
-
-        return Notification.objects.create(user=user, notification_type=notification_type, **validated_data)
+    # def create(self, validated_data):
+    #     content_object = validated_data['content_object']
+    #     user = self.context['request'].user
+    #
+    #     if isinstance(user, PetSeeker) and isinstance(content_object, Application):
+    #         notification_type = 'status_update'
+    #
+    #     # if user is PetShelter AND content_object refers to Application => application creation notification
+    #     elif isinstance(user, PetShelter) and isinstance(content_object, Application):
+    #         notification_type = 'application_creation'
+    #
+    #     # if user is PetShelter AND content_object refers to Comment AND comment.content_object refers to Shelter
+    #     elif isinstance(user, PetShelter) and isinstance(content_object, Comment) and isinstance(content_object.content_object, PetShelter):
+    #         notification_type = 'new_review'
+    #
+    #     # if content_object refers to Comment AND comment.content_object refers to Application => new message notification
+    #     elif isinstance(content_object, Comment) and isinstance(content_object.content_object, Application):
+    #         notification_type = 'new_message'
+    #
+    #     # if content_object refers to Pet => new pet listing notification
+    #     elif isinstance(content_object, Pet):
+    #         notification_type = 'new_pet_listing'
+    #
+    #     # if no conditions apply set an empty value
+    #     else:
+    #         notification_type = ''
+    #
+    #     return Notification.objects.create(user=user, notification_type=notification_type, **validated_data)
--- petpal/notifications/views.py
+++ petpal/notifications/views.py
@@ -3,11 +3,13 @@ from applications.models import Application
 from pets.models import Pet
 from .models import Notification
 from django.urls import reverse_lazy
-from tokenize import Comment
 from .serializers import NotificationSerializer
 from rest_framework.generics import CreateAPIView, UpdateAPIView, ListAPIView, DestroyAPIView, RetrieveAPIView
 from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404
+from comments.models import Comment
+from django.http import Http404
+
 
 class NotificationCreate(CreateAPIView):
     """
@@ -37,31 +39,33 @@ class NotificationCreate(CreateAPIView):
         # serializer.validated_data['content_object'] = content_object
         content_type = serializer.validated_data.get('content_type')
         object_id = serializer.validated_data.get('object_id')
+        try:
+            content_object = content_type.get_object_for_this_type(id=object_id)
+        except:
+            raise Http404
 
-
-
-        content_object = content_type.get_object_for_this_type(id=object_id)
-
+        print(ContentType.objects.get_for_model(Comment).id)
         user = self.request.user
 
         # if user is PetSeeker AND content_object refers to Application => status update notification
-        if isinstance(user, PetSeeker) and isinstance(content_object, Application):
+        if user.is_pet_seeker() and content_type.model == 'application':
             notification_type = 'status_update'
         
         # if user is PetShelter AND content_object refers to Application => application creation notification
-        elif isinstance(user, PetShelter) and isinstance(content_object, Application):
+        elif user.is_pet_shelter() and content_type.model == 'application':
             notification_type = 'application_creation'
 
         # if user is PetShelter AND content_object refers to Comment AND comment.content_object refers to Shelter
-        elif isinstance(user, PetShelter) and isinstance(content_object, Comment) and isinstance(content_type, PetShelter):
+        elif (user.is_pet_shelter() and content_type.model == 'comment'
+              and content_object.content_type.model == 'petshelter'):
             notification_type = 'new_review'
-        
+
         # if content_object refers to Comment AND comment.content_object refers to Application => new message notification
-        elif isinstance(content_object, Comment) and isinstance(content_type, Application):
+        elif content_type.model == 'comment' and content_object.content_type.model == 'application':
             notification_type = 'new_message'
-            
+
         # if content_object refers to Pet => new pet listing notification
-        elif isinstance(content_object, Pet):
+        elif content_type.model == 'pet':
             notification_type = 'new_pet_listing'
         
         # if no conditions apply set an empty value
@@ -71,13 +75,13 @@ class NotificationCreate(CreateAPIView):
         # create and save the notification with the user and notification type
         serializer.save(user=user, notification_type=notification_type)
 
-    def post(self, request, *args, **kwargs):
-        # creating a new notification
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return self.response(serializer.data, headers=headers)
+    # def post(self, request, *args, **kwargs):
+    #     # creating a new notification
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_create(serializer)
+    #     headers = self.get_success_headers(serializer.data)
+    #     return self.response(serializer.data, headers=headers)
 
 class NotificationUpdate(UpdateAPIView):
     """
