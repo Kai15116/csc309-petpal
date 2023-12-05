27a99ddc573069050865a55e7a692a8512bedcb4
kai <kai.dicarlantonio@mail.utoronto.ca>
2023-12-05 13:30:22 -0500

    Added pagination for my pets

--- frontend/src/pages/MyPets.js
+++ frontend/src/pages/MyPets.js
@@ -1,11 +1,31 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useMemo, useState} from 'react';
 import '../styles/shelterManagement.css'
 import LandingHeader from "../components/LandingHeader";
 import Footer from "../components/Footer";
 import {userContext} from "../context/userContext";
 import SimplePetCard from "../components/SimplePetCard";
 import ShelterCard from "../components/ShelterCard";
-import {useNavigate, useParams} from "react-router-dom";
+import {useNavigate, useParams, useSearchParams} from "react-router-dom";
+import CPagination from "../components/CPagination";
+
+// Reference Lecture example: URL parser.
+function to_url_params(object) {
+    var result = [];
+    for (const key in object) {
+        if (Array.isArray(object[key])) {
+            for (const value of object[key]) {
+                result.push(`${key}[]=${value}`);
+            }
+        }
+        else {
+            let value = object[key];
+            if (value !== "") {
+            result.push(`${key}=${value}`);
+            }
+        }
+    }
+    return result.join('&');
+}
 
 const MyPets = () => {
     const { getContextUser, setContextUser} = useContext(userContext);
@@ -15,6 +35,15 @@ const MyPets = () => {
     const [userInfo, setUserInfo] = useState(null);
     const navigate = useNavigate();
     const { shelterId } = useParams();
+
+    const [ searchParams, setSearchParams ] = useSearchParams();
+    const query = useMemo(() => ({
+        page : parseInt(searchParams.get("page") ?? 1),
+        size: parseInt(searchParams.get("size") ?? 12),
+    }), [searchParams]);
+    const pagesCount = Math.ceil(myPets?.count / query?.size);
+    const noResult = myPets?.count === 0 || isNaN(pagesCount);
+
     let id;
     if (shelterId)
         id = shelterId
@@ -22,17 +51,23 @@ const MyPets = () => {
         id = user?.contextUserId
     const isOwner = user?.contextUserId === Number(shelterId) || !shelterId
 
+    // used for pagination state management
+    const setcurrentActivePage = (value) => {
+        setSearchParams({...query, page : value})
+    }
+
     useEffect(function () {
+        const urlParams = to_url_params({...query, owner: id});
         async function fetchPets() {
             try {
-                const response = await fetch(`http://localhost:8000/pets?owner=${id}`, {
+                const response = await fetch(`http://localhost:8000/pets?${urlParams}`, {
                     method: 'GET',
                 });
 
                 if (response.status >= 200 && response.status < 300) {
                     const data = await response.json();
                     console.log(data)
-                    setMyPets([...data])
+                    setMyPets({...data})
                 } else if (response.status === 404) {
                     alert(404);
                 } else {
@@ -100,9 +135,12 @@ const MyPets = () => {
             </div>
             <hr></hr>
             <div className="d-grid" id="pets-grid">
-                {myPets?.map((pet, index) => <SimplePetCard key={index} size="12rem" {...pet}></SimplePetCard>)}
+                {myPets?.results?.map((pet, index) => <SimplePetCard key={index} size="12rem" {...pet}></SimplePetCard>)}
             </div>
-            {myPets?.length === 0 && <h3 className="ms-2 text-secondary">No pets found . . . </h3>}
+              {!noResult && <div className="mt-4">
+                   <CPagination setcurrentActivePage={setcurrentActivePage} currentActivePage={query?.page} pagesCount={pagesCount}/>
+              </div>}
+            {noResult && <h3 className="ms-2 text-secondary">No pets found . . . </h3>}
           </div>
         </div>
       </div>
