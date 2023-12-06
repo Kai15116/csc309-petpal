3983ecff4010ec330057369415610a3410ad60f0
Charlie Tao <charlietaoxl@gmail.com>
2023-12-05 19:21:26 -0500

    Seeker Profile

--- frontend/src/components/profile/SeekerProfileDetails.js
+++ frontend/src/components/profile/SeekerProfileDetails.js
@@ -5,33 +5,39 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Props should be userInfo and userId object to display appropriate information
-function ShelterProfileDetailsCard(props) {
+function SeekerProfileDetailsCard(props) {
     const userId = props.userId;
     const userInfo = props.userInfo;
     const [contactOpen, setContactOpen] = useState(false);
     const navigate = useNavigate();
 
     return (
-        <Card style={{ position: "relative", bottom: "9rem", maxWidth: "30rem"}}>
-            <Card.Body className="pb-2" >
+        <Card className="mt-3" style={{ maxWidth: "30rem"}}>
+            <Card.Body className="pb-3" >
                 <Card.Title> 
-                    <h3>{userInfo?.mission_title} </h3>
+                    <h3> About {userInfo?.username} {' '}
+                    {   // Check if viewer is same as page owner
+                        (true) && (
+                            <a href="" onClick={() => { navigate("/shelterprofileedit") }}>
+                                <i className="bi bi-pencil-square"></i>
+                            </a>
+                        )
+                    }
+
+                    </h3>
                 </Card.Title>
                 <Card.Subtitle className="text-muted" style={{ fontSize: "14px" }}> 
                     {userInfo?.address} 
                 </Card.Subtitle>
                 <Card.Text className="mt-2"> 
-                    {userInfo?.description}
-                    {/* Since you have no small description of the shelter Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    {/* {userInfo?.description} */}
+                    Since you have no small description of the shelter Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Natus aliquam dignissimos officia, ducimus voluptatem blanditiis in, ullam neque adipisci 
-                    facere delectus aut nisi porro, nemo nihil quidem autem laudantium? Perspiciatis. */}
+                    facere delectus aut nisi porro, nemo nihil quidem autem laudantium? Perspiciatis.
                 </Card.Text>
-                <div className="mb-2" style={{ display: "flex", justifyContent: "space-evenly"}}>
-                    <Button variant="primary" onClick={() => navigate(`/mypets/`)}>
-                        View Pets
-                    </Button>
+                <div className="mb-2">
                     <Button 
-                        variant="outline-dark" 
+                        variant="outline-primary" 
                         onClick={() => setContactOpen(!contactOpen)}
                         aria-controls="shelter-contact-collapse" 
                         aria-expanded={contactOpen}
@@ -43,9 +49,9 @@ function ShelterProfileDetailsCard(props) {
                     <div className="" >
                         <Card className="m-0 p-0" id="shelter-contact-collapse">
                             <Card.Body>
-                                <p>Email: {userInfo?.email}</p>
-                                <p>Phone: {userInfo?.phone_number}</p>
-                                <p>Site: {userInfo?.website}</p>
+                                { (userInfo?.email) && (<p>Email: {userInfo?.email}</p>)}
+                                { (userInfo?.phone_number) && (<p>Phone: {userInfo?.phone_number}</p>)}
+                                { (userInfo?.website) && (<p>Site: {userInfo?.website}</p>)}
                             </Card.Body>
                         </Card>
                     </div>
@@ -55,4 +61,4 @@ function ShelterProfileDetailsCard(props) {
     );
 }
 
-export default ShelterProfileDetailsCard;
+export default SeekerProfileDetailsCard;
