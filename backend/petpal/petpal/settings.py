9915b71f78bf05b30aad81b1abf0b34072fab5c6
kai <kai.dicarlantonio@mail.utoronto.ca>
2023-12-05 22:21:38 -0500

    changed database settings

--- backend/petpal/petpal/settings.py
+++ backend/petpal/petpal/settings.py
@@ -100,11 +100,8 @@ WSGI_APPLICATION = 'petpal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("POSTGRES_DATABASE"),
-        'USER': env("POSTGRES_USER"),
-        'PASSWORD': env("POSTGRES_PASSWORD"),
-        'HOST': env("POSTGRES_HOST"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
